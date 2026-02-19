32be48c2671ead78dd3130d6e584b613b1a9fa63
Ayanna Negi <ayanna.negiii@gmail.com>
2026-02-19 10:51:52 +0530

    Q8.c
    Refactor comments for clarity in power function.

--- Day8.c
+++ Day8.c
@@ -3,13 +3,9 @@
 #include <stdio.h>
 
 int power(int a, int b) {
-    // Base case
-    if (b == 0) return 1;
-    
-    // Recursive case
+    if (b == 0) return 1;         //base case
     return a * power(a, b - 1);
 }
-
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
