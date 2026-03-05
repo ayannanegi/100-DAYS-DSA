69259aad963e59b108df666f2ca69ac0d82fc902
Ayanna Negi <ayanna.negiii@gmail.com>
2026-03-05 10:27:08 +0530

    Update DAY5.c

--- DAY5.c
+++ DAY5.c
@@ -1,6 +1,6 @@
 //Problem: A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Your task is to create a single chronological log that preserves the correct order of arrivals.
 //Input: - First line: integer p (number of entries in server log 1) - Second line: p sorted integers representing arrival times from server 1 - Third line: integer q (number of entries in server log 2) - Fourth line: q sorted integers representing arrival times from server 2) Output: - Print a single line containing all arrival times in chronological order, separated by spaces
-
+ 
 #include <stdio.h>
 int main() {
     int p, q;
