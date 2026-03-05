ac01a5a86b8f4a7175d2df7d0ef81b68f5dd3ec9
Ayanna Negi <ayanna.negiii@gmail.com>
2026-03-05 10:45:11 +0530

    Update Day11.c

--- Day11.c
+++ Day11.c
@@ -1,6 +1,5 @@
 //Problem: Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.
 #include <stdio.h>
-
 int main() {
     int m, n;
     int mat1[100][100], mat2[100][100], result[100][100];
