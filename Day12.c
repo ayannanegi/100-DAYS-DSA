0ea4ffef2b3896033ceba0bc6546df7536776a80
Ayanna Negi <ayanna.negiii@gmail.com>
2026-03-05 10:45:52 +0530

    Update Day12.c

--- Day12.c
+++ Day12.c
@@ -4,9 +4,7 @@
 int main() {
     int m, n;
     int mat[100][100];
-    
     scanf("%d %d", &m, &n);
-    
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &mat[i][j]);
