ee676b2153b643eebfb0d6afe96802c2b98a8edd
Ayanna Negi <ayanna.negiii@gmail.com>
2026-04-05 23:32:46 +0530

    Create Day-46.c

---
+++ Day-46.c
@@ -0,0 +1,13 @@
+//Implement the solution for this problem.
+#include <stdio.h>
+#include <stdlib.h>
+
+// Level Order Traversal implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
