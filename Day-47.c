6d8d3fa9ba1fbe3b3ce9cde6f10884786c95d049
Ayanna Negi <ayanna.negiii@gmail.com>
2026-04-08 23:38:08 +0530

    Create Day-47.c

---
+++ Day-47.c
@@ -0,0 +1,13 @@
+//Implement the solution for this problem.
+#include <stdio.h>
+#include <stdlib.h>
+
+// Height of Binary Tree implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
