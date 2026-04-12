95e7bdc1d87ab36ede07f5bf636226a5112c11f6
Ayanna Negi <ayanna.negiii@gmail.com>
2026-04-12 21:56:20 +0530

    Create Day-60.c

---
+++ Day-60.c
@@ -0,0 +1,22 @@
+//Check whether a given binary tree satisfies the Min-Heap property.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+int isMinHeap(struct Node* root) {
+    if(root == NULL)
+        return 1;
+
+    if(root->left != NULL && root->data > root->left->data)
+        return 0;
+
+    if(root->right != NULL && root->data > root->right->data)
+        return 0;
+
+    return isMinHeap(root->left) && isMinHeap(root->right);
+}
